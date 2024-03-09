81b35b5da7d57f9eff9c4b08d9eb680caf6bdbad
KlimovaED <klimovaed@icloud.com>
2024-03-09 15:28:25 +0100

    1

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -5,6 +5,15 @@ import {
     number,btnActive,btnNext,btnPrev,LinkLearnMore
 } from "./vars";
 import {
+    nameInput,
+    emailInput,
+    telephoneInput,
+    errorName,
+    errorEmail,
+    errorTelephone,
+    formMessage
+} from "./vars";
+import {
     validateEmail,
     validateName,
     validatePerson,
@@ -200,3 +209,70 @@ window.addEventListener('click', function (event) {
         number.value ="";
     }
 });
+
+//форма с главной страницы
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', sendForm);
+    async function sendForm(e) {
+        e.preventDefault();
+        //имя
+        if (!nameInput.value) {
+            nameInput.classList.add('error');
+            errorName.textContent = 'Введите имя';
+
+        }
+        //эмейл
+        if (!emailInput.value) {
+            emailInput.classList.add('error');
+            errorEmail.textContent = 'Введите эмейл';
+        }
+        //телефон
+        if (!telephoneInput.value) {
+            telephoneInput.classList.add('error');
+            errorTelephone.textContent = 'Введите номер телефона';
+        }
+        //валидация эмейла
+        if (!validateEmail(emailInput.value)) {
+            errorEmail.textContent = 'эмейл не содержит всех необходимых символов. Проверьте еще раз!'
+        }
+        //валидация телефона
+        if (!validateTelephone(telephoneInput.value)) {
+            errorTelephone.textContent = 'Некорректный номер телефона. Пример: +7(904)567-89-43';
+        }
+        else {
+            //имя
+            nameInput.classList.remove('error');
+            errorName.textContent = '';
+            console.log(nameInput.value);
+            //Записываем имя в localstorage
+            window.localStorage.setItem('name', JSON.stringify(nameInput.value));
+            //эмейл
+            emailInput.classList.remove('error');
+            errorEmail.textContent = '';
+            console.log(emailInput.value);
+            //Записываем эмейл в localstorage
+            window.localStorage.setItem('email', JSON.stringify(emailInput.value));
+            //телефон
+            telephoneInput.classList.remove('error');
+            errorTelephone.textContent = '';
+            console.log(telephoneInput.value);
+            //Записываем телефон в localstorage
+            window.localStorage.setItem('phone', JSON.stringify(telephoneInput.value));
+            //сообщение о том что форма отправлена
+            formMessage.textContent = 'Данная форма отправлена';
+            //очищаем форму
+            form.reset();
+        }
+    }
+    //функция теста email
+    function validateEmail(input) {
+        let regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return regexEmail.test(input);
+    }
+    //функция проверки на мобильный номер
+    function validateTelephone(phone) {
+        let regexTel = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
+        return regexTel.test(phone);
+    }
+})
--- src/calendar/vars.js
+++ src/calendar/vars.js
@@ -6,15 +6,18 @@ export const btnActive = document.getElementById("btn__active");
 export const btnNext = document.getElementById("btn__next");
 export let LinkLearnMore = document.getElementById("link__learn-more");
 
-/*export  const btnInfo = document.getElementById("info__btn");
-export  const modal = document.getElementById("modal");
-export const btnClose = document.getElementsByClassName("modal__close")[0];
-export  const confirmForm= document.forms.confirmForm;
-export  const inputs = document.querySelectorAll('.input');
-export  let hasError = false;*/
 
 export  let name = confirmForm.elements.name;
 export  let tel = confirmForm.elements.tel;
 export  let email = confirmForm.elements.email;
 export  let number = confirmForm.elements.number;
+//записываем инпуты в переменные
+export const nameInput = document.getElementById('name');
+export const emailInput = document.getElementById('email');
+export const telephoneInput = document.getElementById('telephone');
+//переменные для вывода ошибок и сообщений
+export const errorName = document.getElementById('error-name');
+export const errorEmail = document.getElementById('error-email');
+export const errorTelephone = document.getElementById('error-telephone');
+export const formMessage = document.getElementById('form-message');
    
