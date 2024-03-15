19325ac77f6fbfdd4ec5bf25f0fcf9767ae0634d
VarvaraBel <varvara.bielova@mail.ru>
2024-03-15 23:23:17 +0300

    weatherapi

--- src/tour-page.js
+++ src/tour-page.js
@@ -11,3 +11,36 @@ function makeCLick() {
     })
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = '85f30b8095b94031bbf193620241503';
+    const city = 'Москва'; // Здесь должно быть название города из файла tours.json
+    const url = 'http://api.weatherapi.com/v1/current.json?key=' + apiKey + '&q=' + city;
+    const weatherElement = document.getElementById('weather');
+
+    // Функция для получения погоды
+    function getWeather() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+    
+        xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 400) {
+            const response = JSON.parse(xhr.responseText);
+            const temperature = response.current.temp_c;
+
+          // Вывод данных
+        weatherElement.textContent = temperature + '°C';
+        } else {
+        console.error('Ошибка запроса. Код статуса: ' + xhr.status);
+        }
+    };
+    
+    xhr.onerror = function() {
+        console.error('Ошибка запроса');
+    };
+    
+    xhr.send();
+    }
+
+    // Вызов функции для получения погоды
+    getWeather();
+});
